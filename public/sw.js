a61f2c336014ff647185b4e8bf1862db3a200df5
dani-sie <dani-sie@uniandes.edu.co>
2026-09-09 13:21:58 -0500

    Refresh service worker cache

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pmp-swipe-v1";
+const CACHE_NAME = "pmp-swipe-v2";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest"];
 
 self.addEventListener("install", (event) => {
